515f84cad177d784b75eec78273b356cbe7dafc4
Konstantin <kos8533@yandex.ru>
2021-01-07 12:23:40 +0300

    Initial commit

---
+++ Client/src/main/java/controllers/MainWindowController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class MainWindowController {
+
+}
